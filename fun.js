b3243bfc2112f85ebbefcc768060f76edb2e8b70
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-11 10:06:34 +0300

    first commit

---
+++ fun.js
@@ -0,0 +1,49 @@
+//function  that takes in strings
+let food = ("eating")
+unction (strings)
+    for(let u=0;j<strings.length; u++){
+
+    
+   console.log(strings[j]);  
+    }
+    let strings=["eating"]
+    .reverse(strings);
+
+
+
+
+
+
+function takesArray(arrNum);{
+    let arrNum ;(i=0; (i<arrNum));
+    console.log(arrNum)
+}
+let nums =[2,8,0,23,5,45,76];
+console.log(nums);
+
+
+
+
+//function array of nums
+function arrayOfnums(arrnums){
+    let array;(j=0 (j<arrNum))
+    console.log(arrnums)
+
+        
+}
+let arrnums =[12,87,45,75,23,64,73];
+console.log(arrnums *4);
+
+
+function arrOfstrings(){
+    returnarr(Element[nums]);
+    console.log(Strings);{
+        strings = ("janet","jane","shan");
+
+    }
+    console.return (string[nums]);
+
+}
+let Elementnums = [10,24,45,56,67]
+console .log(nums);
+
